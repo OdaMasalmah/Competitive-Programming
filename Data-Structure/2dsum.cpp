1a7cf2823d9ce3f24b4783bee4445b3ae43069b0
Masalmah <49412099+OdaMasalmah@users.noreply.github.com>
2020-09-10 02:59:23 +0300

    Update 2dsum.cpp

--- Data-Structure/2dsum.cpp
+++ Data-Structure/2dsum.cpp
@@ -1,17 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define pr(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
-#define all(v) (v).begin(), (v).end()
-#define int long long
-#define sz(v) (int)(v).size()
-#define mod 1000000007 // ((a- b)% mod+ mod)% mod
-
-void err(istream_iterator<string> it) {}
-template<typename T, typename... Args>
-void err(istream_iterator<string> it, T a, Args... args) {
-	cerr << *it << " = " << a << endl;
-	err(++it, args...);
-}
 
 const int N= 1001;
 
@@ -59,8 +47,7 @@ void solve() {
         cout << ans<<"\n";
 }
 
-int32_t main(){
-ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL);
+int main(){
         int T= 1;
         cin >> T;
         while ( T-- ) solve();
