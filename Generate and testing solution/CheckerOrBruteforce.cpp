d0271aa76746d6153b9ecd5d63a4cc09a4328dd0
Masalmah <49412099+OdaMasalmah@users.noreply.github.com>
2020-09-10 20:39:22 +0300

    Create CheckerOrBruteforce.cpp

---
+++ Generate and testing solution/CheckerOrBruteforce.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) (v).begin(), (v).end()
+#define int long long
+#define sz(v) (int)(v).size()
+#define mod 1000000007 // ((a- b)% mod+ mod)% mod
+
+
+void solve(int tt) {
+
+}
+
+void test_result() { // Write code to test the output
+        freopen("C:\\Users\\lenovo\\Desktop\\Desktop\\test_Masalmah\\out.txt", "r", stdin); // file to read the answer
+
+}
+
+void output_result() { // Write Brute force solution
+        freopen("C:\\Users\\lenovo\\Desktop\\Desktop\\test_Masalmah\\in.txt", "r", stdin); // read the generate test
+        freopen("C:\\Users\\lenovo\\Desktop\\Desktop\\test_Masalmah\\out2.txt", "w", stdout); // file to store this code answer
+}
+
+int32_t main(){
+ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL);
+
+        //output_result();
+        test_result();
+
+        int T= 100;
+        //cin >> T;
+        for (int i = 0; i< T; i++) {
+                solve(i);
+        }
+return 0;
+}
