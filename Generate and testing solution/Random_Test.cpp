1c1486e87359ff477dbb9cf5a989f98c78681bac
Masalmah <49412099+OdaMasalmah@users.noreply.github.com>
2020-09-17 15:45:05 +0300

    Update Random_Test.cpp

--- Generate and testing solution/Random_Test.cpp
+++ Generate and testing solution/Random_Test.cpp
@@ -3,28 +3,21 @@ using namespace std;
 /*
 Generate test cases
 */
-int rand(int a, int b) {
-        return a+ rand()% (b- a+ 1);
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int rand(int l, int r){
+	uniform_int_distribution<int> uid(l, r);
+	return uid(rng);
 }
 
 int main() {
 
-        freopen("C:\\Users\\lenovo\\Desktop\\Desktop\\test_Masalmah\\in.txt", "w", stdout); // output file
+        freopen("C:\\Users\\lenovo\\Desktop\\Desktop\\test_Masalmah\\in.txt", "w", stdout);
 
-        srand(time(0));
-
-        int T= 100;
-        cout << T<< "\n";
-
-        while ( T-- ) {
-                int n= rand(2, 50);
-
+        int T= 10;
+        while (T--) {
+                int n= rand(1, 10);
                 cout << n<< "\n";
-                for (int i = 0; i< n; i++) {
-                        int x= rand(1, 10);
-                        cout << x% 2 << " \n"[i== n-1];
-                }
-
         }
 return 0;
 }
